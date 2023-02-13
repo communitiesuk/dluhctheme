066e807129f70e07457aafca592f20e3d07f35c9
Niall McSharry <niall.mcsharry@levellingup.gov.uk>
2023-02-13 13:58:07 +0000

    Documented Help to Buy dataset

---
+++ R/Help_to_Buy.R
@@ -0,0 +1,13 @@
+#' Help to Buy Completion data
+#'
+#' A subset of data from the Help to Buy Quarterly statistical release
+#' Q4 2021, produced by DLUHC
+#'
+#' @format ## `Help_to_Buy`
+#' A data frame with 334 rows and 2 columns:
+#' \describe{
+#'   \item{LA_Code}{Local Authority ONS Code}
+#'   \item{Completions}{The number of Help to Buy completions recorded in the local authority between April 2012 and December 2021}
+#' }
+#' @source <https://www.gov.uk/government/statistics/help-to-buy-equity-loan-scheme-data-to-31-december-2021>
+"Help_to_Buy"
